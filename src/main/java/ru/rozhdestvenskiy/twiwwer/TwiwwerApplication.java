03201a08eba812714d2754ef54362ea9b882fce3
Andrey Rozhdestvenskiy <jobrozhdestvenskiy@yandex.ru>
2022-12-24 16:06:14 +0300

    Initial commit

---
+++ src/main/java/ru/rozhdestvenskiy/twiwwer/TwiwwerApplication.java
@@ -0,0 +1,13 @@
+package ru.rozhdestvenskiy.twiwwer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwiwwerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwiwwerApplication.class, args);
+	}
+
+}
